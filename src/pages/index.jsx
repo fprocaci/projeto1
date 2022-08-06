ef64a08994aa4d0c40f73c8ae8a79cd33e29ed28
Fabricio Procaci <fprocaci@gmail.com>
2022-08-05 23:07:50 -0300

    Atualizando

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import MainContent from "../../components/main/MainContent";
 import MenuAside from "../../components/menuAside/menuAside";
-import MenuMobile from "../../components/menuMobile/MenuMobile";
 import Navmobile from "../../components/navMobile/NavMobile";
 import SideL from "../../components/sideL/SideL";
 import SideR from "../../components/sideR/SideR";
@@ -12,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Grid</title>
+        <title>Mathelda</title>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
