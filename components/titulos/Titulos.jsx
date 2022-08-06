787fc72fb7d8da2eddce74b96dc93324b095cc01
Fabricio Procaci <fprocaci@gmail.com>
2022-08-06 00:04:04 -0300

    Atualizando

--- components/titulos/Titulos.jsx
+++ components/titulos/Titulos.jsx
@@ -1,13 +1,14 @@
-import styles from './Titulos.module.scss'
+import styles from "./Titulos.module.scss";
 
 export default function Titulos() {
-    return (
-        <div className={styles.titulos}>
-            <div className={styles.titulo_principal}>
-                <span>Hello There</span><hr />
-            </div>
+  return (
+    <div className={styles.titulos}>
+      <div className={styles.titulo_principal}>
+        <span>Hello There</span>
+        <hr />
+      </div>
 
-            <h1>breathing pixels</h1>
-        </div>
-    )
+      <h1>breathing pixels</h1>
+    </div>
+  );
 }
